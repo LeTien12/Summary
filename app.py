69fb2b85a8ba1b6034bdb26c29cf61223a232994
TienLe12 <tle38413@gmail.com>
2024-08-14 13:35:56 +0700

    loaddata

---
+++ app.py
@@ -0,0 +1,17 @@
+﻿import streamlit as st
+from tool.funtions import summarize
+
+
+def main():
+
+    st.title('Wellcom to here')
+    user = st.chat_input()
+
+    if user:
+        with st.spinner("Prosessing..."):
+            text = summarize(user)
+            st.write(text)
+            
+if __name__ == '__main__':
+    main()
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+﻿from setuptools import find_packages , setup
+
+setup(
+    name = "Summary",
+    version =  '0.0.1',
+    author= 'TienLe',
+    author_email='tle38413@gmail.com',
+    install_requires = ["streamlit" , "transformers"],
+    packages=find_packages()
+)
